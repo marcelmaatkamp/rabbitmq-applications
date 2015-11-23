cb7575647bc121ccc1ddb76cf40d90666adfb118
Marcel Maatkamp <m.maatkamp@gmail.com>
2015-11-23 16:34:37 +0100

    get

--- application/search/consumer/src/main/java/org/application/rabbitmq/search/consumer/configuration/search/SearchConfiguration.java
+++ application/search/consumer/src/main/java/org/application/rabbitmq/search/consumer/configuration/search/SearchConfiguration.java
@@ -17,6 +17,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -27,6 +31,12 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Configuration
 public class SearchConfiguration {
     private static final Logger log = LoggerFactory.getLogger(SearchConfiguration.class);
+
+    /**
+     * System.getProperties().put( "proxySet", "true" );
+       System.getProperties().put( "socksProxyHost", "127.0.0.1" );
+       System.getProperties().put( "socksProxyPort", "1234" );
+     */
 
     private final AtomicInteger counter = new AtomicInteger();
 
@@ -47,7 +57,30 @@ public class SearchConfiguration {
             value = @Queue(value = "url", durable = "true"),
             exchange = @Exchange(value = "url"))
     )
-    public void process(String message) {
-        log.info("message("+message+")");
+    public void process(URL url) throws IOException {
+        log.info("url("+url+")");
+        sendGET(url);
+
+    }
+    private static void sendGET(URL url) throws IOException {
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", "google");
+        int responseCode = con.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            // print result
+            log.info(response.toString());
+        } else {
+            log.info("GET request not worked");
+        }
+
     }
 }
