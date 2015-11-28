c28586086c6160bfe9b8f6148fb103ba79d643b7
Marcel Maatkamp <m.maatkamp@gmail.com>
2015-11-28 20:27:34 +0100

    exchanger

---
+++ application/datadiode/exchanger/internal/src/main/java/org/application/rabbitmq/datadiode/exchanger/configuration/internal/ExchangerInternalConfiguration.java
@@ -0,0 +1,10 @@
+package org.application.rabbitmq.datadiode.exchanger.configuration.internal;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by marcel on 28-11-15.
+ */
+@Configuration
+public class ExchangerInternalConfiguration {
+}
