efb69b6f14e46d3dabc0601c03b677d0430a0c09
Marcel Maatkamp <m.maatkamp@gmail.com>
2015-12-13 16:57:06 +0100

    node

--- application/datadiode/udp/internal/contrib/udp.js
+++ application/datadiode/udp/internal/contrib/udp.js
@@ -1,6 +1,12 @@
 var dgram = require('dgram');
+var amqp = require("amqp-ts");
+
+var connection = new amqp.Connection("amqp://localhost:5674");
+var exchange = connection.declareExchange("udp");
+
 var s = dgram.createSocket('udp4');
 s.bind(9999);
 socket.on('message', function(msg, rinfo) {
-    console.log('Received %d bytes from %s:%d\n', msg.length, rinfo.address, rinfo.port);
+    // console.log('Received %d bytes from %s:%d\n', msg.length, rinfo.address, rinfo.port);
+    exchange.send(msg);
 });
