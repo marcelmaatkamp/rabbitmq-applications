7f850e6a031c3e60963798aef6915b57600dd3fc
Marcel Maatkamp <m.maatkamp@gmail.com>
2015-12-13 21:14:22 +0100

    buffers

--- application/datadiode/red/contrib/network.sh
+++ application/datadiode/red/contrib/network.sh
@@ -4,13 +4,14 @@ arp -s 192.168.1.1 00:25:90:eb:e0:9e
 sudo sysctl -w \
   net.core.rmem_max=86214400 \
   net.core.wmem_max=26214400 \
-  net.core.rmem_default=924288 \
+  net.core.rmem_default=26214400 \
   net.core.wmem_default=924288 \
   fs.file-max=100000 \
   vm.swappiness=10 \
   net.core.optmem_max=80960 \
   net.core.netdev_max_backlog=70000 \
-  net.ipv4.udp_rmem_min=32768 \
+  net.ipv4.udp_mem='26214400 26214400 26214400' \
+  net.ipv4.udp_rmem_min=26214400 \
   net.ipv4.udp_wmem_min=32768 \
   net.ipv4.conf.all.send_redirects=0 \
   net.ipv4.conf.all.accept_redirects=0 \
--- application/datadiode/udp/internal/contrib/server.c
+++ application/datadiode/udp/internal/contrib/server.c
@@ -42,8 +42,9 @@ int main(void) {
     }
 
     // amqp
-    char const *hostname = "192.168.178.13";
-    int port = 32770, status;
+    // char const *hostname = "192.168.178.13"; int port = 32770;
+    char const *hostname = "localhost"; int port = 5674;
+    int status;
     amqp_socket_t *socket = NULL;
     amqp_connection_state_t conn;
     conn = amqp_new_connection();
