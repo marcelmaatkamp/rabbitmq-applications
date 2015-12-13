1aa26b86221881ec5fc38e584d31f77cf9bb9e07
Marcel Maatkamp <m.maatkamp@gmail.com>
2015-12-13 17:17:01 +0100

    fanout

--- application/datadiode/udp/internal/contrib/udp.js
+++ application/datadiode/udp/internal/contrib/udp.js
@@ -2,7 +2,7 @@ var dgram = require('dgram');
 var amqp = require("amqp-ts");
 
 var connection = new amqp.Connection("amqp://localhost:5674");
-var exchange = connection.declareExchange("udp");
+var exchange = connection.declareExchange("udp", "fanout");
 
 var socket = dgram.createSocket('udp4');
 socket.bind(9999);
