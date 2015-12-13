d11c3f42b9009816f2eb429564d7160c71b6eedb
Marcel Maatkamp <m.maatkamp@gmail.com>
2015-12-13 19:29:05 +0100

    c imll

--- application/datadiode/udp/internal/contrib/server.c
+++ application/datadiode/udp/internal/contrib/server.c
@@ -1,71 +1,77 @@
-/*
-    Simple udp server
-    Silver Moon (m00n.silv3r@gmail.com)
-*/
 #include<stdio.h> //printf
 #include<string.h> //memset
 #include<stdlib.h> //exit(0);
 #include<arpa/inet.h>
 #include<sys/socket.h>
- 
-#define BUFLEN 512  //Max length of buffer
-#define PORT 8888   //The port on which to listen for incoming data
- 
-void die(char *s)
-{
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <amqp_tcp_socket.h>
+#include <amqp.h>
+#include <amqp_framing.h>
+
+#define BUFLEN 8192  //Max length of buffer
+#define PORT 9999   //The port on which to listen for incoming data
+
+void died(char *s) {
     perror(s);
     exit(1);
 }
- 
-int main(void)
-{
+
+int main(void) {
     struct sockaddr_in si_me, si_other;
-     
+
     int s, i, slen = sizeof(si_other) , recv_len;
     char buf[BUFLEN];
-     
-    //create a UDP socket
-    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
-    {
-        die("socket");
+
+    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+        died("socket");
     }
-     
-    // zero out the structure
+
     memset((char *) &si_me, 0, sizeof(si_me));
-     
+
     si_me.sin_family = AF_INET;
     si_me.sin_port = htons(PORT);
     si_me.sin_addr.s_addr = htonl(INADDR_ANY);
-     
+
     //bind socket to port
-    if( bind(s , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1)
-    {
-        die("bind");
+    if( bind(s , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1) {
+        died("bind");
     }
-     
-    //keep listening for data
-    while(1)
-    {
+
+    // amqp
+    char const *hostname = "localhost";
+    int port = 5674;
+    amqp_socket_t *socket = NULL;
+    amqp_connection_state_t conn;
+    conn = amqp_new_connection();
+    socket = amqp_tcp_socket_new(conn);
+    if (!socket) {
+      die("creating TCP socket");
+    }
+    status = amqp_socket_open(socket, hostname, port);
+    if (status) {
+      die("opening TCP socket");
+    }
+
+    die_on_amqp_error(amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "guest", "guest"), "Logging in");
+    amqp_channel_open(conn, 1);
+    die_on_amqp_error(amqp_get_rpc_reply(conn), "Opening channel");
+
+    while(1) {
         printf("Waiting for data...");
         fflush(stdout);
-         
-        //try to receive some data, this is a blocking call
-        if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1)
-        {
-            die("recvfrom()");
+
+        if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1) {
+            died("recvfrom()");
         }
-         
-        //print details of the client/peer and the data received
+
         printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
         printf("Data: %s\n" , buf);
-         
-        //now reply the client with the same data
-        if (sendto(s, buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == -1)
-        {
-            die("sendto()");
-        }
     }
- 
+
     close(s);
     return 0;
 }
