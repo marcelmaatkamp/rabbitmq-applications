3cc5798ed9cecd72f64e1226748225bf94bef3de
Marcel Maatkamp <m.maatkamp@gmail.com>
2015-12-13 08:13:53 +0100

    duurtest

--- application/datadiode/udp/src/main/java/org/udp/client/fast/Client.java
+++ application/datadiode/udp/src/main/java/org/udp/client/fast/Client.java
@@ -23,9 +23,17 @@ public class Client {
 
     // (8192 * 1024 * 256)/1024/1024 = 2048 = 2G
     int packetSize = 8192;
-    int packetCount = 1024 * 128;
+    int packetCount = 1024 * 1024 * 64;
 
-    int packetRate = 14500;
+    // int packetRate = 59500; // transferred 1048576 packets of 8192 bytes = 8192 MB in 17.598 secs = 59585.0 pkts/sec or 466.0 MB/sec = (100/1048576)* 1048234 = 99.967384338 
+    // int packetRate = 79500; // transferred 1048576 packets of 8192 bytes = 8192 MB in 13.175 secs = 79588.0 pkts/sec or 622.0 MB/sec = (100/1048576)*1048117 = 99.95% 
+    // int packetRate = 99550; // transferred 1048576 packets of 8192 bytes = 8192 MB in 10.53 secs = 99580.0 pkts/sec or 778.0 MB/sec = (100/1048576)*1047737 = 99.91 %
+    // int packetRate = 120500; // transferred 1048576 packets of 8192 bytes = 8192 MB in 8.694 secs = 120609.0 pkts/sec or 942.0 MB/sec = (100/1048576)* 1046631 = 99.81 %
+    // int packetRate = 145000; // transferred 1048576 packets of 8192 bytes = 8192 MB in 7.224 secs = 145152.0 pkts/sec or 1134.0 MB/sec = (100/1048576)* 1046958 = 99.85%   
+
+     // max = transferred 8388608 packets of 8192 bytes = 65536 MB in 55.441 secs = 151307.0 pkts/sec or 1182.0 MB/sec, received 8379876, (8388608-8379876) = 8732 packets lost = (100/8388608)*8379876 = 99.89%
+     // duurtest: transferred 67108864 packets of 8192 bytes = 524288 MB in 447.257 secs = 150045.0 pkts/sec or 1172.0 MB/sec = 56637 = 99.91%
+    int packetRate = 150000;  
 
     Client() throws UnknownHostException, SocketException {
         InetAddress ia = InetAddress.getByName(hostname);
