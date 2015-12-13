ccc6c79be1bcd6e55fccc51939957a9958101c9c
Marcel Maatkamp <m.maatkamp@gmail.com>
2015-12-13 16:50:23 +0100

    print

--- application/datadiode/udp/internal/contrib/udp.js
+++ application/datadiode/udp/internal/contrib/udp.js
@@ -1,3 +1,6 @@
 var dgram = require('dgram');
 var s = dgram.createSocket('udp4');
 s.bind(9999);
+socket.on('message', function(msg, rinfo) {
+    console.log('Received %d bytes from %s:%d\n', msg.length, rinfo.address, rinfo.port);
+});
